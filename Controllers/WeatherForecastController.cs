969a014a530ce0feae1a117cb49d02678893a5c0
Michał Hadław <w66237@student.wsiz.edu.pl>
2022-10-20 12:55:23 +0200

    pierwszy commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Serwices;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public int Get()
+        {
+            var calculateInstance = new Calculate(23);
+            calculateInstance.Increase(46);
+            return calculateInstance.GetValue();
+        }
+    }
+}
---
+++ Serwices/Calculate.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication1.Serwices
+{
+    public class Calculate
+    {
+        public Calculate(int value1)
+        {
+            Value = value1;
+
+        }
+        private int Value { get; set; }
+        public void Increase(int value)
+        {
+            value += value;
+        }
+
+        public void Decrease(int value)
+        {
+            value -= value;
+        }
+        public int GetValue()
+        {
+            return Value;
+        }
+    }
+}
